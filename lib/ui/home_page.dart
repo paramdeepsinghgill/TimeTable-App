9a008be029543ddadc768f66d5b9162cb49df717
paramdeepsinghgill <paramdeepsinghgill03@gmail.com>
2022-11-01 00:58:52 +0530

    Added pages

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -12,41 +12,128 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('Time Table TIET')
-        ),
+        appBar: AppBar(title: Text('Time Table TIET')),
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Image.asset('assets/logo.png'),
-                  Text(
-                      "In which year do you belong to ?"),
-                  Container(height: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        
-                      },
-                      child: const Text("First Year")),
-                  Container(height: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        
-                      },
-                      child: const Text("Second Year")),
-                  Container(height: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        
-                      },
-                      child: const Text("Third Year")),
-                  Container(height: 10),
-                  Text(
-                      "Paramdeep Singh Gill, Roll no.: 102103258, B.Tech.(Computer Engineering)"),
-                  Container(height: 10),
-            ])
-        )
-    );
+              Image.asset('assets/logo.png'),
+              Text("In which year do you belong to ?"),
+              Container(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Screen2()));
+                  },
+                  child: const Text("First Year")),
+              Container(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Screen3()));
+                  },
+                  child: const Text("Second Year")),
+              Container(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Screen4()));
+                  },
+                  child: const Text("Third Year")),
+              Container(height: 10),
+              Text(
+                  "Paramdeep Singh Gill, Roll no.: 102103258, B.Tech.(Computer Engineering)"),
+              Container(height: 10),
+            ])));
   }
 }
 
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('First Year'), backgroundColor: Colors.red),
+        body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ConstrainedBox(
+                constraints: BoxConstraints(),
+                child: Column(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Table(
+                      defaultColumnWidth: FixedColumnWidth(120.0),
+                      border: TableBorder.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 2),
+                    ),
+                  ),
+                ]))));
+  }
+}
+
+class Screen3 extends StatefulWidget {
+  @override
+  _Screen3State createState() => _Screen3State();
+}
+
+class _Screen3State extends State<Screen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Second Year'), backgroundColor: Colors.red),
+        body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ConstrainedBox(
+                constraints: BoxConstraints(),
+                child: Column(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Table(
+                      defaultColumnWidth: FixedColumnWidth(120.0),
+                      border: TableBorder.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 2),
+                    ),
+                  ),
+                ]))));
+  }
+}
+
+class Screen4 extends StatefulWidget {
+  @override
+  _Screen4State createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Third Year'), backgroundColor: Colors.red),
+        body: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ConstrainedBox(
+                constraints: BoxConstraints(),
+                child: Column(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Table(
+                      defaultColumnWidth: FixedColumnWidth(120.0),
+                      border: TableBorder.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 2),
+                    ),
+                  ),
+                ]))));
+  }
+}
